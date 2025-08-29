e6de3aca15575f2ca83c61cd3b020aa6abc7df3d
Mellad Sultani <140553611+takebackreality@users.noreply.github.com>
2025-08-28 23:31:24 -0700

    Create config.h

---
+++ rev_firmware/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define BAUD_RATE 115200
+
+#define SERVO_PAN_PIN 9
+#define SERVO_TILT_PIN 10
+#define FIRE_PIN 7
+
+#define PAN_MIN 0
+#define PAN_MAX 180
+#define TILT_MIN 30
+#define TILT_MAX 150
+
+#define FIRE_PULSE_MS 200
+
+#endif
